41dcfff91e63b49f7b9a56baa19dd67929618077
dipudangol2 <dipudangol2@gmail.com>
2023-12-15 23:02:41 +0545

    Add first web scraper which operates on a html file for demonstration of BeautifulSoup

---
+++ web-scraping/scraper1/main1.py
@@ -0,0 +1,16 @@
+'''Demo code for reading data from home.html file 
+using BeautifulSoup library and scraping and using 
+the required data from the file as we want'''
+
+from bs4 import BeautifulSoup
+
+with open('home.html', 'r') as html_file:
+    content = html_file.read()
+    
+    soup = BeautifulSoup(content, 'lxml')
+    course_cards = soup.find_all('div',class_='card')
+    for course in course_cards:
+        course_name = course.h5.text
+        course_price = course.a.text.split()[-1]
+        
+        print(f'{course_name} costs {course_price}')
