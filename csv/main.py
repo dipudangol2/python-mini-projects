d4388fbc2ba24a4dd9202a9ec93571ae57763724
dipudangol2 <dipudangol2@gmail.com>
2023-12-17 22:29:50 +0545

    Remove unnecessary docstring and add 'r' mode while opening csv file on cal_average

--- csv/main.py
+++ csv/main.py
@@ -7,7 +7,7 @@ import csv
 
 
 def cal_average(age_group, sum_age, sum_grade):
-    with open("students.csv", newline="") as csvfile:
+    with open("students.csv",'r', newline="") as csvfile:
         reader = csv.DictReader(csvfile)
         num_row = len(list(reader))
         csvfile.seek(0)
@@ -65,52 +65,3 @@ average_age = sum_age / num_row
 average_grade = sum_grade / num_row
 print(f"The average age of students is {average_age} and the average grade is {average_grade}")
 write_with_age_group(age_group)
-
-
-"""
-with open("students.csv", newline="") as csvfile:
-    reader = csv.DictReader(csvfile)
-    num_row = len(list(reader))
-    print(num_row)
-    csvfile.seek(0)
-    reader = csv.DictReader(csvfile)
-    print(reader.fieldnames)
-    # field_names = reader.fieldnames
-    # for key in field_names:
-    #     print(key)
-    for row in reader:
-        if row["age"].isdigit() and row["grade"].isdigit():
-            sum_age += int(row["age"])
-            sum_grade += int(row["grade"])
-            if int(row["age"]) >= 15 and int(row["age"]) < 20:
-                age_group["15-20"].append(row)
-            if int(row["age"]) >= 20 and int(row["age"]) < 25:
-                age_group["20-25"].append(row)
-            if int(row["age"]) >= 25 and int(row["age"]) < 30:
-                age_group["25-30"].append(row)
-            if int(row["age"]) >= 30 and int(row["age"]) < 35:
-                age_group["30-35"].append(row)
-            print(row["name"], row["age"], row["grade"])
-            
-            print(age_group)
-for group in age_group.keys():
-    print(age_group[group])
-    for lists in age_group[group]:
-        print(lists)
-
-
-
-with open("age_group.csv", "w", newline="") as csvfile:
-    writer = csv.writer(csvfile)
-    header = ["name", "age-group", "grade"]
-    writer.writerow(header)
-    for group in age_group.keys():
-        for lists in age_group[group]:
-            new_row = []
-            new_row.append(lists["name"])
-            new_row.append(group)
-            new_row.append(lists["grade"])
-            # print(new_row)
-            writer.writerow(new_row)
-
-"""
